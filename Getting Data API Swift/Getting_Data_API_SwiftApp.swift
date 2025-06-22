66dda5850c3a007a0affa914c2471bfe8f5bb651
Jasmin Hachmane <509304@student.fontys.nl>
2025-06-22 19:22:10 +0200

    Initial Commit

---
+++ Getting Data API Swift/Getting_Data_API_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Getting_Data_API_SwiftApp.swift
+//  Getting Data API Swift
+//
+//  Created by Jasmin Hachmane on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Getting_Data_API_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
